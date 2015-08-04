9d80fd5a0654b719cbd97bd8b40de50080747449
Cecilie Strand <strcec@gmail.com>
2015-08-04 15:41:41 +0200

    Kode

---
+++ Kokebok/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kokebok.Startup))]
+namespace Kokebok
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
