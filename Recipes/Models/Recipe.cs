59833d6c403337e303f494248054e6b993a7711c
Cecilie Strand <strcec@gmail.com>
2015-08-04 15:43:29 +0200

    Ny model

---
+++ Recipes/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recipes.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Instructions { get; set; }
+    }
+}
